eb7ef08d393b1b33744ffe87b05c71bda7dde73a
SaiKumar Immadi <immadisaikumar@ymail.com>
2017-08-12 12:18:15 +0530

    sentiment analysis file done

---
+++ Cloud-Computing/lab2/Sentiment.py
@@ -0,0 +1,20 @@
+import sys
+import ast
+SentimentLexiconFile=open("SentimentLexicon.txt","r")
+SentimentLexicon={}
+for WordSentiment in SentimentLexiconFile:
+	temp_list=WordSentiment.split()
+	SentimentValue=ast.literal_eval(temp_list[-1])
+	temp_list=temp_list[:-1]
+	SentimentWord=' '.join(temp_list)
+	SentimentLexicon[SentimentWord]=SentimentValue
+#sentiment_lexicon={"happy":4,"sad":-4,"awesome":5,"fanatstic":5,"super":3,"angry":-3,"cry":-5,"laugh":5,"smile":2,"negative":-1,"simple":0}              
+SentenseWords=sys.argv[1:][0].split()
+sentiment=0
+for word in SentenseWords:
+	if word in SentimentLexicon:
+		sentiment+=SentimentLexicon[word]
+	else:
+		sentiment+=0
+print "The sentiment of the sentence",sys.argv[1:][0],"is",sentiment
+#python Setiment.py "angry cry laugh hate smile"
